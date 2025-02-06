ff00098ac65ab73e2403795dbf50ff89fc7a53c9
Bongibault Romain <bongibault.romain@gmail.com>
2025-02-06 14:58:27 +0100

    unkown

--- scrapping/scrapper/src/main/java/lt/bongibau/scrapper/Scrapper.java
+++ scrapping/scrapper/src/main/java/lt/bongibau/scrapper/Scrapper.java
@@ -6,10 +6,30 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Scrapper {
     public static void main(String[] args) throws IOException, InterruptedException {
-        SearchManager.getInstance().start(5);
+        URL test = new URL("https://google.com/search?q=java");
+        HttpURLConnection connection = (HttpURLConnection) test.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+
+        System.out.println(test.para);
     }
 }
