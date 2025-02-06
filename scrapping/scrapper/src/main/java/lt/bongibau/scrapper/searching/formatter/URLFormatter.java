fe3e7933b639b12a578ec6ede07bbdbc720c8720
jploubejac <jeanphiloubejac@orange.fr>
2025-02-06 18:39:22 +0100

    add: URLFormatter

---
+++ scrapping/scrapper/src/main/java/lt/bongibau/scrapper/searching/formatter/URLFormatter.java
@@ -0,0 +1,12 @@
+package lt.bongibau.scrapper.searching.formatter;
+
+import java.net.URL;
+
+public class URLFormatter {
+    static URL format(URL url){
+        return url;
+    }
+    static URL hrefToUrl(URL baseUrl, String href){
+        return baseUrl;
+    }
+}
