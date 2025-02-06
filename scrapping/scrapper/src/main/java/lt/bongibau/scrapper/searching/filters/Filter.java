9e53077cdf973f1bd201e93c660479add0d9b643
Bongibault Romain <bongibault.romain@gmail.com>
2025-02-06 18:03:27 +0100

    feat: upgrade filters

--- scrapping/scrapper/src/main/java/lt/bongibau/scrapper/searching/filters/Filter.java
+++ scrapping/scrapper/src/main/java/lt/bongibau/scrapper/searching/filters/Filter.java
@@ -1,6 +1,7 @@
 package lt.bongibau.scrapper.searching.filters;
 
 import java.net.URL;
+import java.util.Arrays;
 
 /**
  * Represents a filter for URLs, which can be used to accept or deny URLs
@@ -18,6 +19,19 @@ public record Filter(String host, lt.bongibau.scrapper.searching.filters.Filter.
      * @return true if the URL is accepted by the filter
      */
     public boolean check(URL url) {
-        return url.getHost().contains(host);
+        String[] urlDomains = url.getHost().split("\\.");
+        String[] hosts = host.split("\\.");
+
+        if (hosts.length > urlDomains.length) {
+            return false;
+        }
+
+        for (int i = hosts.length - 1; i >= 0; i--) {
+            if (!hosts[i].equals(urlDomains[i + urlDomains.length - hosts.length])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
