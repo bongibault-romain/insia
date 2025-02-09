b73fc55d579cd509c118cb17a94eebb09a7a2a07
Bongibault Romain <bongibault.romain@gmail.com>
2025-02-09 12:52:33 +0100

    test

--- models/gpt.py
+++ models/gpt.py
@@ -31,10 +31,13 @@ with open(data_path, 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
-vocab_size = encoding.max_token_value
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
 # create a mapping from characters to integers
-encode = lambda s: encoding.encode(s)
-decode = lambda l: encoding.decode(l)
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
