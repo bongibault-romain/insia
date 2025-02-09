a3f6d1b894bfd61dca2d58c0edbe0715d9284c99
Bongibault Romain <bongibault.romain@gmail.com>
2025-02-09 12:34:46 +0100

    test

--- models/ddp/test.py
+++ models/ddp/test.py
@@ -3,6 +3,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 import os
+import torch.multiprocessing as mp
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -16,36 +17,28 @@ class ToyModel(nn.Module):
     def forward(self, x):
         return self.net2(self.relu(self.net1(x)))
 
-
-def demo_basic():
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
-    dist.init_process_group("nccl")
-    rank = dist.get_rank()
-    print(f"Start running basic DDP example on rank {rank}.")
-    # create model and move it to GPU with id rank
-    device_id = rank % torch.cuda.device_count()
-    print(f"Using GPU {device_id}")
-    model = ToyModel().to(device_id)
-    print(f"Model created on rank {rank}.")
-    ddp_model = DDP(model, device_ids=[device_id])
-    print(f"DDP model created on rank {rank}.")
-    loss_fn = nn.MSELoss()
-    print(f"Loss function created on rank {rank}.")
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-    print(f"Optimizer created on rank {rank}.")
-
-    optimizer.zero_grad()
-    print(f"Optimizer zeroed on rank {rank}.")
-    outputs = ddp_model(torch.randn(20, 10))
-    print(f"Outputs created on rank {rank}.")
-    labels = torch.randn(20, 5).to(device_id)
-    print(f"Labels created on rank {rank}.")
-    loss_fn(outputs, labels).backward()
-    print(f"Loss backwarded on rank {rank}.")
-    optimizer.step()
-    print(f"Optimizer stepped on rank {rank}.")
+def setup(rank, world_size):
+    dist.init_process_group("nccl")  # Backend pour GPU, "gloo" pour CPU
+    torch.cuda.set_device(rank)
+    
+def cleanup():
     dist.destroy_process_group()
-    print(f"Finished running basic DDP example on rank {rank}.")
+
+def train(rank, world_size):
+    print(f"Running DDP on rank {rank}.")
+    setup(rank, world_size)
+    print(f"Rank {rank} is using GPU {torch.cuda.current_device()}.")
+    model = ToyModel().to(rank)
+    print(f"Model on rank {rank} is on GPU {next(model.parameters()).device}.")
+    model = DDP(model, device_ids=[rank])
+    print(f"Model on rank {rank} is on GPU {next(model.parameters()).device}.")
+
+    # Votre boucle d'entraînement ici...
+
+    print(f"Rank {rank} is done.")
+    cleanup()
+    print(f"Rank {rank} has cleaned up.")
 
 if __name__ == "__main__":
-    demo_basic()
+    world_size = torch.cuda.device_count()
+    mp.spawn(train, args=(world_size,), nprocs=world_size, join=True)
